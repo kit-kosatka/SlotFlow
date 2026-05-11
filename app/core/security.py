8f3a64f164892e1abb557133d35346de930e4a55
Dmitry <dimon_barkov@icloud.com>
2026-05-11 14:56:07 +0300

    add security and config

---
+++ app/core/security.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from fastapi import HTTPException
+from app.core.config import settings
+from jose import jwt, JWTError
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed.decode()
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
+
+def create_token(user_id: int) -> str:
+    payload = {
+        "sub": user_id,
+        "exp": datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    }
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return token
+
+def verify_token(token: str) -> str:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return payload
+
+
+
